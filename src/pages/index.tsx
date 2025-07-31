6efbea8ad046fa9fe6c766cc21efc80780e56ac8
Leslie-Archibald <lm.archibald.17@gmail.com>
2025-07-31 19:42:09 -0400

    Added scaffolding for starter page
    Created layout.tsx, which houses shared navbar coding
    Created index.tsx, which houses website landing page
    Altered App.tsx to route to layout.tsx and index.tsx in order to render the pages

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+const Index = () => {
+    return (
+        <>
+            <h1>Successfully rendered new Page</h1>
+        </>
+    )
+}
+
+export default Index
---
+++ src/pages/layout.tsx
@@ -0,0 +1,22 @@
+// file for common layout elements 
+
+import {Outlet, Link} from "react-router-dom";
+
+const Layout = () => {
+    return (
+        <>
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/bio">Bio</Link></li>
+                <li><Link to="/projects">Projects</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+            </ul>
+        </nav>
+
+        <Outlet />
+        </>
+    )
+};
+
+export default Layout;
